5c25ffe27a47ce0bbdf37aca03332ce1bccc919c
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-08-29 14:02:20 +0300

    My application for Assignment #6 created

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,115 +1,247 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+Color mydefaultColor = Color.fromARGB(255, 27, 108, 30);
+bool Op1 = false;
+bool Op2 = false;
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        home: Scaffold(
+            backgroundColor: mydefaultColor,
+            drawer: Drawer(),
+            appBar: AppBar(
+              backgroundColor: Colors.black12,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            body: ListView(
+              // physics: ,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    //action coe when button is pressed
+                    setState(() {
+                      mydefaultColor == Color.fromARGB(255, 27, 108, 30)
+                          ? mydefaultColor = Color.fromARGB(255, 178, 249, 181)
+                          : mydefaultColor = Color.fromARGB(255, 27, 108, 30);
+                    });
+                  },
+                  icon: Icon(Icons.dark_mode,
+                      color: Color.fromARGB(255, 255, 254, 254)),
+                ),
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    "Check what you like Us yo post more please .",
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        CheckboxListTile(
+                            secondary: CircleAvatar(
+                              child: Icon(
+                                Icons.nature_people,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                              backgroundColor: Color.fromARGB(0, 255, 193, 7),
+                            ),
+                            title: Text(
+                              "Option 1 ",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              "Earth's Atmosphere",
+                              style: TextStyle(
+                                  fontSize: 23,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            isThreeLine: true,
+                            checkColor: Color.fromARGB(255, 255, 255, 255),
+                            value: Op1,
+                            onChanged: (value) {
+                              setState(() {
+                                Op1 = value!;
+                              });
+                            }),
+                        CheckboxListTile(
+                            secondary: CircleAvatar(
+                              child: Icon(
+                                Icons.nature_people,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                              backgroundColor: Color.fromARGB(0, 255, 193, 7),
+                            ),
+                            title: Text(
+                              "Option 2 ",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              "Eclipses",
+                              style: TextStyle(
+                                  fontSize: 23,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            isThreeLine: true,
+                            checkColor: Color.fromARGB(255, 254, 254, 254),
+                            value: Op2,
+                            onChanged: (value) {
+                              setState(() {
+                                Op2 = value!;
+                              });
+                            }),
+                      ],
+                    ),
+                  )
+                ]),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/1.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/4.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/5.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/6.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/7.jpg"),
+                    ),
+                  ),
+                ),
+                Container(
+                
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  child: ListTile(
+                    title: const Text(
+                      'Other Images :',
+                      style: TextStyle(
+                          fontSize: 20, color: Color.fromARGB(255, 3, 3, 3)),
+                    ),
+                    tileColor: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
+                Container(
+                    height: 300,
+                    child: ListView(
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(
+                          width: 350,
+                          height: 500,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("images/2.jpg"),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.arrow_circle_right,
+                              color: Color.fromARGB(255, 255, 254, 254)),
+                        ),
+                        Container(
+                          width: 350,
+                          height: 500,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("images/3.jpg"),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.arrow_circle_right,
+                              color: Color.fromARGB(255, 255, 254, 254)),
+                        ),
+                        Container(
+                          width: 350,
+                          height: 500,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("images/3.jpg"),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.arrow_circle_right,
+                              color: Color.fromARGB(255, 255, 254, 254)),
+                        ),
+                        Container(
+                          width: 350,
+                          height: 500,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("images/8.jpg"),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ],
+            )));
   }
 }
